6d61865f598d87a32af555ffb666ed98961027fa
rifqi <rifqym40@gmail.com>
2019-11-25 11:36:27 +0700

    blog

---
+++ admin/index.php
@@ -0,0 +1,22 @@
+<!-- Header -->
+<?php include('../layouts/includes/head.php');?>
+<!-- End Header -->
+<body class="app header-fixed sidebar-fixed aside-menu-fiked sidebar-lg-show">
+<!-- Navbar -->
+<?php include('../layouts/includes/navbar.php');?>
+<!-- End Navbar -->
+<div class = "app-body">
+<!--Sidebar  -->
+<?php include('../layouts/includes/sidebar.php');?>
+<!-- End Sidebar -->
+<!-- MainKonten -->
+<main class ="main"></main>
+<!-- End MinKonten -->
+</div>
+<?php include('../layouts/includes/footer.php');?>
+<!-- coreUI and necessary plugins -->
+<!-- Scripts -->
+<?php include('../layouts/includes/scripts.php');?>
+<!-- End Scripts -->
+</body>
+</html>
